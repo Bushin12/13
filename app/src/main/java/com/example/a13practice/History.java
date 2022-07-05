b4517bdf4f2fcf599db5bfa22aa455b481429efd
bushin1999 <bushin1999@bk.ru>
2022-07-05 13:31:28 +0700

    1

---
+++ app/src/main/java/com/example/a13practice/History.java
@@ -0,0 +1,13 @@
+package com.example.a13practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
---
+++ app/src/main/java/com/example/a13practice/History_1.java
@@ -0,0 +1,14 @@
+package com.example.a13practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class History_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history1);
+    }
+}
